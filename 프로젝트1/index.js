619e86ee6af7baeef873c1815c0e48b2dc0a74f8
jdh5772 <82750373+jdh5772@users.noreply.github.com>
2024-08-13 15:04:03 +0900

    Add files via upload

--- 프로젝트1/index.js
+++ 프로젝트1/index.js
@@ -1,14 +1,33 @@
-// const API_KEY = '';
+const API_KEY = '487a4c46786d6f6f39336b6e4c6f51';
 const $navItems = document.querySelectorAll('.nav-item');
 const $select = document.getElementById('select');
 const $searchInput = document.querySelector('.searchInput');
 const $searchButton = document.querySelector('.searchBtn');
+const $loading = document.querySelector('.loading');
+const $cards = document.getElementById('cards');
+const $hamBtn = document.querySelector('.ham');
+const $modal = document.querySelector('.modal');
+const $modalHam = document.querySelector('.modal-ham');
+const $modalCategory = document.querySelector('.modal-category');
 
 let currentPage = 1;
-let totalDataNum;
+let totalDataNum = 0;
 let currentCategory = ' ';
 let pageSize = 9;
 let groupSize = 10;
+let currentTitle = ' ';
+let currentDate = ' ';
+let resizing = false;
+
+const showLoading = ()=>{
+    $loading.style.display = 'block';
+    $cards.style.display = 'none';
+}
+
+const hideLoading = ()=>{
+    $loading.style.display = 'none';
+    $cards.style.display = 'grid';
+}
 
 const createHtml = li=>{
     const img = li['MAIN_IMG'] || './img/no_image.png';
@@ -29,26 +48,54 @@ const createHtml = li=>{
 
 const renderList = list=>{
     const html = list.map(li=>createHtml(li)).join('');
-    document.getElementById('cards').innerHTML = html;
+    $cards.innerHTML = html;
 };
+
 
 const fetchData = async (category=" ",title=" ",date=" ")=>{
     try{
-        const startIdx = currentPage*(currentPage-1)+1;
-        const endIdx = currentPage*pageSize;
+        showLoading();
+        const startIdx = pageSize*(currentPage-1)+1;
+        const endIdx = pageSize*currentPage;
         let url = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/${startIdx}/${endIdx}/${category}/${title}/${date}`;
 
         const res = await fetch(url);
         const data = await res.json();
-
-        totalDataNum = data.culturalEventInfo['list_total_count'];
-        const list = data.culturalEventInfo.row;
-        renderList(list);
+        if(!res.ok){
+            throw new Error('연결 실패');
+        }
+        
+        if(data.culturalEventInfo && data.culturalEventInfo.RESULT.MESSAGE === '정상 처리되었습니다'){
+            totalDataNum = data.culturalEventInfo['list_total_count'];
+            const list = data.culturalEventInfo.row;
+            renderList(list);
+        } else{
+            $cards.innerHTML = `
+            <li>
+            <p>데이터가 없습니다.</p>
+            </li>
+            `;
+            totalDataNum = 0;
+        }
         pagination();
+
     }
     catch(e){
         console.log(e);
+        $cards.innerHTML = `
+        <li>
+        <p>데이터를 불러오는데 실패했습니다.</p>
+        </li>
+        `;
     }
+    finally{
+        hideLoading();
+    }
+}
+
+const movePage = pageNum=>{
+    currentPage = pageNum;
+    fetchData(currentCategory,currentTitle,currentDate);
 }
 
 const pagination = ()=>{
@@ -60,47 +107,58 @@ const pagination = ()=>{
     const nextGroupfirst = currentPageGroup * groupSize + 1;
 
     let html = `
-    <button class="prevGroup" ${currentPageGroup===1 ? 'disabled' : ''}><<</button>
+    <button class="prevGroup" ${currentPageGroup===1 ? 'disabled' : ''} onClick="movePage(${prevGroupFirst})"><<</button>
     `;
 
     html+= `
-    <button class="prevPage" ${currentPageGroup===1 ? 'disabled' : ''}><</button>
+    <button class="prevPage" ${currentPage===1 ? 'disabled' : ''} onClick="movePage(${currentPage-1})"><</button>
     `;
 
     for(let i=currentGroupFIrstPage;i<=currentGroupLastPage;i++){
         html+=`
-        <button class="${i===currentPageGroup ? 'on' : ''}">${i}</button>
+        <button class="${i===currentPage ? 'on' : ''}" onClick="movePage(${i})">${i}</button>
         `;
     }
 
     html+=`
-    <button class="nextPage" ${currentPage>= endPage ? 'disabled' : ''}>></button>
+    <button class="nextPage" ${currentPage>= endPage ? 'disabled' : ''} onClick="movePage(${currentPage+1})">></button>
     `;
 
     html+=`
-    <button class="nextGroup" ${currentPageGroup * groupSize >= endPage ? 'disabled' : ''}>>></button>
+    <button class="nextGroup" ${currentPageGroup * groupSize >= endPage ? 'disabled' : ''} onClick="movePage(${nextGroupfirst})">>></button>
     `
 
     document.querySelector('.pgCon').innerHTML = html;
 }
 
+
 const searchData = ()=>{
+    currentPage = 1;
     const selectValue = $select.value;
     const inputValue = $searchInput.value;
+    if(inputValue===''){
+        return;
+    }
+
     if(selectValue==='search-title'){
-        fetchData(' ',inputValue);
+        currentTitle = inputValue;
+        currentDate = ' ';
+        fetchData(currentCategory,inputValue,' ');
     } else{
+        currentDate = inputValue;
+        currentTitle = ' ';
         const rgx = /^\d{4}-\d{2}-\d{2}$/;
         if(rgx.test(inputValue)){
-            fetchData(' ',' ',inputValue);
+            fetchData(currentCategory,' ',inputValue);
         } else{
-            alert('9999-99-99 형식으로 입력하세요!');
+            alert('2024-08-12 형식으로 입력하세요!');
+            $searchInput.value = '';
         }
     }
-    $searchInput.value = '';
 }
 
-const handleClick = e=>{
+const handleItemClick = e=>{
+    e.preventDefault();
     const category = e.currentTarget.dataset.category;
     const onLink = document.querySelector('.gnb .on');
     if(onLink){
@@ -108,23 +166,65 @@ const handleClick = e=>{
     }
     e.currentTarget.classList.add('on');
     currentCategory = category;
+    currentTitle = ' ';
+    currentDate = ' ';
+    currentPage = 1;
+    $searchInput.value = '';
     fetchData(currentCategory);
 }
 
+const handleSelectChange = e => {
+    if(e.currentTarget.value==='search-title'){
+        $searchInput.setAttribute('placeholder','공연/행사명 입력');
+    } else{
+        $searchInput.setAttribute('placeholder','날짜 입력 ex)2024-08-12');
+    }
+}
+
+
+const handleWindowResize = ()=>{
+    if(resizing){
+        return ;
+    }
+    resizing = true;
+
+    if(window.innerWidth<=900){
+        pageSize = 6;
+        groupSize = 5;
+    } else{
+        pageSize = 9;
+        groupSize = 10;
+    }
+
+    setTimeout(() => {
+        fetchData(currentCategory,currentTitle,currentDate);
+        pagination();
+        resizing = false;
+    }, 300);
+}
+
+const toggleModal = boolean=>{
+    $modal.classList.toggle('on',boolean);
+}
+
+const clickModalCategory = e=>{
+    currentCategory = e.target.dataset.category;
+    currentTitle = ' ';
+    currentDate = ' ';
+    toggleModal(false);
+    fetchData(currentCategory,' ',' ');
+    pagination();
+}
+
+
+
 $navItems.forEach(item=>{
     item.addEventListener('click',e=>{
-        e.preventDefault();
-        handleClick(e);
+        handleItemClick(e);
     })
 });
 
-$select.addEventListener('change',e=>{
-    if(e.currentTarget.value==='search-title'){
-        $searchInput.setAttribute('placeholder','공연 및 행사명을 입력하세요.');
-    } else{
-        $searchInput.setAttribute('placeholder','날짜를 입력하세요. ex)2024-08-12');
-    }
-})
+$select.addEventListener('change',handleSelectChange);
 
 $searchButton.addEventListener('click',searchData);
 $searchInput.addEventListener('keyup',e=>{
@@ -132,5 +232,17 @@ $searchInput.addEventListener('keyup',e=>{
         searchData();
     }
 })
+
+window.addEventListener('resize',handleWindowResize);
+
+$hamBtn.addEventListener('click',()=>{
+    toggleModal(true);
+})
+
+$modalHam.addEventListener('click',()=>{
+    toggleModal(false);
+})
+
+$modalCategory.addEventListener('click',e=>clickModalCategory(e));
 
 fetchData();
