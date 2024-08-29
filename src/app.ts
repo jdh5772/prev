b04dc6aa3c035c5e9e013b319340526d96200c5c
jdh5772 <moonstne@naver.com>
2024-08-29 14:43:44 +0900

    ts express 구조 개편

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const app = express();
 
 app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+app.set('views',path.join(__dirname,'../views'));
 
 app.get('/',(req,res)=>{
     res.render('index');
