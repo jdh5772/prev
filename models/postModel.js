577b1a4030e38723cd733e0908aad6c9408e1f9a
jdh5772 <moonstne@naver.com>
2024-09-01 18:52:18 +0900

    댓글 기능 작성중

--- models/postModel.js
+++ models/postModel.js
@@ -1,13 +1,15 @@
 const connectDB = require("../config/db");
-const { ObjectId } = require('mongodb');
+const { ObjectId } = require("mongodb");
+const moment = require("moment-timezone");
 
 let db;
-connectDB.then((client) => {
-    db = client.db("forum");
-})
-.catch(e=>{
-    console.log(e);
-});
+connectDB
+    .then((client) => {
+        db = client.db("forum");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
 
 class Post {
     static async getAllPosts() {
@@ -20,42 +22,54 @@ class Post {
         }
     }
 
-    static async addPost(title,content,username,userUUID){
-        try{
-            await db.collection('posts').insertOne({title,content,username,userUUID});
-        }
-        catch(e){
+    static async addPost(title, content, username, userUUID) {
+        try {
+            const now = moment.tz("Asia/Seoul").format('YYYY-MM-DD HH:mm:ss');
+            await db
+                .collection("posts")
+                .insertOne({
+                    title,
+                    content,
+                    username,
+                    userUUID,
+                    createdAt: now
+                });
+        } catch (e) {
             console.log(e);
             throw e;
         }
     }
 
-    static async getPost(id){
-        try{
-            const post = await db.collection('posts').findOne({_id:new ObjectId(id)});
+    static async getPost(id) {
+        try {
+            const post = await db
+                .collection("posts")
+                .findOne({ _id: new ObjectId(id) });
             return post;
-        }
-        catch(e){
+        } catch (e) {
             console.log(e);
             throw e;
         }
     }
 
-    static async updatePost(id,title,content){
-        try{
-            await db.collection('posts').updateOne({_id:new ObjectId(id)},{$set:{title,content}});
-        }
-        catch(e){
+    static async updatePost(id, title, content) {
+        try {
+            await db
+                .collection("posts")
+                .updateOne(
+                    { _id: new ObjectId(id) },
+                    { $set: { title, content } }
+                );
+        } catch (e) {
             console.log(e);
             throw e;
         }
     }
 
-    static async deletePost(id){
-        try{
-            await db.collection('posts').deleteOne({_id:new ObjectId(id)});
-        }
-        catch(e){
+    static async deletePost(id) {
+        try {
+            await db.collection("posts").deleteOne({ _id: new ObjectId(id) });
+        } catch (e) {
             console.log(e);
             throw e;
         }
--- routes/list.js
+++ routes/list.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {getList, getWrite, postWrite, getDetail, getEdit, postEdit, deletePost} = require('../controllers/listController');
+const {
+    getList,
+    getWrite,
+    postWrite,
+    getDetail,
+    getEdit,
+    postEdit,
+    deletePost,
+    addComments,
+} = require("../controllers/listController");
 
-router.get('/',getList);
-router.get('/write',getWrite);
-router.post('/write',postWrite);
-router.get('/detail/:id',getDetail);
-router.get('/edit/:id',getEdit);
-router.post('/edit/:id',postEdit);
-router.delete('/delete/:id',deletePost);
+router.get("/", getList);
+router.get("/write", getWrite);
+router.post("/write", postWrite);
+router.get("/detail/:id", getDetail);
+router.get("/edit/:id", getEdit);
+router.post("/edit/:id", postEdit);
+router.delete("/delete/:id", deletePost);
+router.post("/comments", addComments);
 
 module.exports = router;
